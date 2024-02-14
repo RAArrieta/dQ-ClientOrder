59950fef0f16a31d91aa6cd15d4210bb572a2aa5
Alfredo Arrieta <alfredoarrieta99@gmail.com>
2024-02-14 13:50:09 -0300

    css

--- src/Components/ClientsOrders/ProductSelector/AddProduct.jsx
+++ src/Components/ClientsOrders/ProductSelector/AddProduct.jsx
@@ -7,6 +7,7 @@ const AddProduct = ({ category }) => {
     cart,
     setCart,
     selectedProducts,
+    setSelectedProducts,
     productQuantities,
     setProductQuantities,
   } = useContext(OrdersContext);
@@ -41,6 +42,12 @@ const AddProduct = ({ category }) => {
         ...prevQuantities,
         [productId]: 1,
       }));
+
+      // Restablecer el producto seleccionado para esta categoría
+      setSelectedProducts((prevSelectedProducts) => ({
+        ...prevSelectedProducts,
+        [category]: "",
+      }));
     }
   };
 
@@ -52,3 +59,89 @@ const AddProduct = ({ category }) => {
 };
 
 export default AddProduct;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useContext } from "react";
+// import { OrdersContext } from "../../../Context/OrdersContext";
+
+// const AddProduct = ({ category }) => {
+//   const {
+//     products,
+//     cart,
+//     setCart,
+//     selectedProducts,
+//     productQuantities,
+//     setProductQuantities,
+//   } = useContext(OrdersContext);
+
+//   const handleAddToCart = (productId) => {
+//     const selectedProduct = products.find(
+//       (product) => product.id === productId
+//     );
+
+//     if (selectedProduct) {
+//       const productIndex = cart.findIndex(
+//         (item) => item.id === productId
+//       );
+//       if (productIndex !== -1) {
+//         setCart((prevCart) => {
+//           const updatedCart = [...prevCart];
+//           updatedCart[productIndex].quantity +=
+//             productQuantities[productId] || 1;
+//           return updatedCart;
+//         });
+//       } else {
+//         setCart((prevCart) => [
+//           ...prevCart,
+//           {
+//             ...selectedProduct,
+//             quantity: productQuantities[productId] || 1,
+//           },
+//         ]);
+//       }
+
+//       setProductQuantities((prevQuantities) => ({
+//         ...prevQuantities,
+//         [productId]: 1,
+//       }));
+//     }
+//   };
+
+//   return (
+//     <button className="btnAgregar" onClick={() => handleAddToCart(selectedProducts[category])}>
+//       Agregar
+//     </button>
+//   );
+// };
+
+// export default AddProduct;
